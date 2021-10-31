0ed437ac8ddccde6894d7a6d32a0084621673be1
Mahdi-RH <Mahdi.roohandeh@hotmail.com>
2021-11-01 00:28:10 +0330

    init project

---
+++ app/src/main/java/com/mohaymen/codeassignment/contacts/presentation/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mohaymen.codeassignment.contacts.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mohaymen.codeassignment.contacts.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
